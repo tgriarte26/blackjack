cebb4a821315ec12d2cc879fcb7d8bc2cb3ac91e
trevor <trevorgriarte@gmail.com>
2024-03-06 22:56:16 -0800

    hi

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -40,19 +40,39 @@ public class Blackjack {
             dealerHandValue += tempValue.getValue();
         }
     }
+    private void placeBets(){
+        System.out.println("You may bet between $10 and $" + playerBank);
+        do{
+            currentBet = kb.nextInt();
+            kb.nextLine();
+            if (currentBet < 10 || currentBet > playerBank) {
+                System.out.println("Invalid bet amount. Please bet between $10 and $" + playerBank);
+            }
+        } while (currentBet < 10 || currentBet > playerBank);
 
-    private int calculateHandValue(ArrayList<Card> hand) {
-        int value = 0;
-        for (Card card : hand) {
-            value += card.getValue();
-        }
-        return value;
     }
 
-    private void placeBets(){
-        System.out.println("You may bet between $0 and $" + playerBank);
-        currentBet = kb.nextInt();
-        kb.nextLine();
+    private void payBack(){
+        // player loses to dealer
+        if(playerHandValue > 21 || playerHandValue < dealerHandValue) {
+            System.out.println("LOSER!. You lost $" + currentBet);
+            playerBank -= currentBet;
+        }
+        // player wins against dealer
+        else if(playerHandValue > dealerHandValue && playerHandValue < 21) {
+            if(playerHandValue == 21) {
+                currentBet *= 1.5;
+                System.out.println("BLACKJACK!. You won $" + currentBet);
+                playerBank *= currentBet;
+            } else {
+                System.out.println("WINNER!. You won $" + currentBet);
+                playerBank += currentBet;
+            }
+        }
+        // player ties with dealer
+        else {
+            System.out.println("PUSH!");
+        }
     }
 
     private void playerTurn() {
@@ -73,41 +93,53 @@ public class Blackjack {
             }
         }
     }
-
     private void dealerTurn(){
         System.out.println("Dealer's hand:\t" + dealer.get(0) + " " + dealer.get(1));
+        while (calculateHandValue(dealer) < 17) {
+            dealer.add(deck.getCard());
+        }
         for (int i = 2; i < dealer.size(); i++) {
             var dealerHand = dealer.get(i);
             System.out.print(" "+ dealerHand);
         }
-        showHand();
-        if(calculateHandValue(dealer) > 21){
-            System.out.println("Dealer busted! You win.");
-            playerBank += currentBet;
-        } else if (calculateHandValue(player) > calculateHandValue(dealer)){
-            System.out.println("You win.");
-            playerBank += currentBet;
-        } else if (calculateHandValue(player) < calculateHandValue(dealer)){
-            System.out.println("You lose");
-            playerBank -= currentBet;
-        } else {
-            System.out.println("Push.");
-        }
+
+        System.out.println();
     }
 
-    private void showHand(){
-        System.out.println("Dealer's hand:\t" + dealer.get(0) + " [?]");
-        System.out.println("Player's hand:\t" + player.get(0) + " " + player.get(1));
+    private int calculateHandValue(ArrayList<Card> hand) {
+        int value = 0;
+        int numAces = 0;
+
+        for (Card card : hand) {
+            int cardValue = card.getValue();
+
+            if (cardValue == 11) {
+                numAces++;
+            }
+
+            value += cardValue;
+        }
+
+        // Handle Aces
+        while (value > 21 && numAces > 0) {
+            value -= 10;
+            numAces--;
+        }
+
+        return value;
     }
+
 
     private void run() {
         while (playerBank > 0){
             placeBets();
             dealCards();
+            System.out.println("Dealer's hand:\t" + dealer.get(0) + " [?]");
+            System.out.print("Player's hand:\t" + player.get(0) + " " + player.get(1));
             playerTurn();
-            if (calculateHandValue(player) <= 21){
-                dealerTurn();
-            }
+            dealerTurn();
+            payBack();
         }
+        System.out.println("Thank you for playing. You are now poor.");
     }
 }
